17621b4b704cd03f513411857470ab90890960a3
kvbxss <kubadownar444@gmail.com>
2023-09-22 16:26:19 +0200

    crud operations in service file

---
+++ server/src/tasks/tasks.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { Task } from "./task.entity";
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(content: string): Task {
+    const task: Task = {
+      id: Date.now(),
+      content,
+      done: false,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTask(id, done: boolean): Task {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      return null;
+    }
+
+    task.done = done;
+    return task;
+  }
+
+  deleteTask(id): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
